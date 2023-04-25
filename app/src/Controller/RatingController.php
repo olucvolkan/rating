4d59c71ed6ce03c4ddb3e8d4fb503c7fecd1f9ab
olucvolkan <volkanoluc@gmail.com>
2023-04-26 00:05:19 +0300

    controller methods added

--- app/src/Controller/RatingController.php
+++ app/src/Controller/RatingController.php
@@ -9,8 +9,35 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/rating', name: 'app_rating')]
 class RatingController extends AbstractController
 {
-    #[Route('/', name: 'create_ra')]
-    public function index(): JsonResponse
+    #[Route('/', name: 'create_rating',methods: ['POST'])]
+    public function create(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/RatingController.php',
+        ]);
+    }
+
+    #[Route('/', name: 'update_rating',methods: ['PUT'])]
+    public function update(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/RatingController.php',
+        ]);
+    }
+
+    #[Route('/', name: 'get_rating',methods: ['GET'])]
+    public function get(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/RatingController.php',
+        ]);
+    }
+
+    #[Route('/', name: 'delete_rating',methods: ['DELETE'])]
+    public function delete(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
